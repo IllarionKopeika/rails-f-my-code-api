32f4dc5ad67b00559eda650d6585197d333b24d3
Illarion Kopeika <larrypearson@yandex.ru>
2022-08-04 18:01:47 +0800

    Add update method

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -1,15 +1,32 @@
 class Api::V1::StoriesController < Api::V1::BaseController
-  before_action :set_story, only: [ :show ]
+  before_action :set_story, only: %i[show update]
 
   def index
-    @stories = Story.all
+    @stories = Story.order(:id)
   end
 
   def show; end
+
+  def update
+    if @story.update(story_params)
+      render :show
+    else
+      render_error
+    end
+  end
 
   private
 
   def set_story
     @story = Story.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:id, :title, :body)
+  end
+
+  def render_error
+    render json: { errors: @story.error.full_messages },
+      status: :unprocessable_entity
   end
 end

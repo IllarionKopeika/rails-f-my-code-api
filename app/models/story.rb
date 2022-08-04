19f0d3413acca6485448dac0fee00e0aabd73d3a
Illarion Kopeika <larrypearson@yandex.ru>
2022-08-04 14:19:38 +0800

    Add seeds

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ApplicationRecord
   has_many :comments
   validates :title, presence: true, uniqueness: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 6 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+stories = [
+  { title: 'Ruby', body: 'Variables, loops, arrays, hashes, methods, interpolations, concatenations, integers and strings...' },
+  { title: 'OOP', body: "Models, controllers, views, repositories, routers, butlers and Zora's castles..." },
+  { title: 'Front-end', body: 'HTML, CSS, divs, cards, containers, classes, Bootstrap, Font Awesome, paddings and margins...' },
+  { title: 'JavaScript', body: 'DOM, events, functions, HTTP, AJAX and VueJS...' },
+  { title: 'Ruby on Rails', body: 'Same as Ruby, but no rake lol' }
+]
+
+Story.create(stories)
